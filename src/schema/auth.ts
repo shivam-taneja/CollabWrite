f2df8ffac8b71523c44fe98d4bc1c53b98276192
Shivam Taneja <shiivamtaneja@gmail.com>
2025-08-29 17:03:34 +0530

    update: auth validation schema to be same as appwrite
    Signed-off-by: Shivam Taneja <shiivamtaneja@gmail.com>

--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -2,13 +2,13 @@ import * as z from 'zod';
 
 export const loginSchema = z.object({
   email: z.email('Please enter a valid email'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
 });
 
 export const signupSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters'),
   email: z.email('Please enter a valid email'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
